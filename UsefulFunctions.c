0a1bc77fb5f30e7223f6c5c182035f68ff53dca7
YourEngineeringSuccess-Support <122680740+YourEngineeringSuccess-Support@users.noreply.github.com>
2023-09-21 16:37:31 +0100

    Making changes to cause a conflict with a pull request.

--- UsefulFunctions.c
+++ UsefulFunctions.c
@@ -50,6 +50,7 @@ void KeepBothChanges ( void )
 	// This line has been added to main/master while you've been developing.
 	// This is a new comment line added via Dev_Steve branch.
 	// This is a new comment line added via Dev_Steve3 branch.
+	// This line has been added to main/master while you've been developing.
 }
 
 /****************************
@@ -65,6 +66,7 @@ void KeepBothChanges ( void )
 void KeepMasterChangesDiscardDev ( void )
 {
 	// Simply add a new line comment line below.
+	// This line has been added to main/master while you've been developing.
 	// This line has been added to main/master while you've been developing.
 }
 
@@ -82,6 +84,7 @@ void KeepDevChangesOverrideMaster ( void )
 	// Simply add a new line comment line below.
 	// This is a new comment line added via Dev_Steve branch.
 	// This is a new comment line added via Dev_Steve3 branch.
+	// This line has been added to main/master while you've been developing.
 }
 
 
