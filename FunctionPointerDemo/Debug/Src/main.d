7b335b53b104df8d5e7e006ca886683065d3d59c
neill lambert <neillsyard@yahoo.com>
2023-12-05 12:06:31 +0000

    provider class settings change

--- FunctionPointerDemo/Debug/Src/main.d
+++ FunctionPointerDemo/Debug/Src/main.d
@@ -1,13 +1,17 @@
-Src/main.o: ../Src/main.c ../inc/STM32F4Headers/stm32f429xx.h \
- ../inc/STM32F4Headers/core_cm4.h ../inc/STM32F4Headers/cmsis_version.h \
- ../inc/STM32F4Headers/cmsis_compiler.h ../inc/STM32F4Headers/cmsis_gcc.h \
- ../inc/STM32F4Headers/mpu_armv7.h \
- ../inc/STM32F4Headers/system_stm32f4xx.h ../Inc/DIOHandler.h
-../inc/STM32F4Headers/stm32f429xx.h:
-../inc/STM32F4Headers/core_cm4.h:
-../inc/STM32F4Headers/cmsis_version.h:
-../inc/STM32F4Headers/cmsis_compiler.h:
-../inc/STM32F4Headers/cmsis_gcc.h:
-../inc/STM32F4Headers/mpu_armv7.h:
-../inc/STM32F4Headers/system_stm32f4xx.h:
+Src/main.o: ../Src/main.c \
+ /home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/stm32f429xx.h \
+ /home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/core_cm4.h \
+ /home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/cmsis_version.h \
+ /home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/cmsis_compiler.h \
+ /home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/cmsis_gcc.h \
+ /home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/mpu_armv7.h \
+ /home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/system_stm32f4xx.h \
+ ../Inc/DIOHandler.h
+/home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/stm32f429xx.h:
+/home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/core_cm4.h:
+/home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/cmsis_version.h:
+/home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/cmsis_compiler.h:
+/home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/cmsis_gcc.h:
+/home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/mpu_armv7.h:
+/home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers/system_stm32f4xx.h:
 ../Inc/DIOHandler.h:
--- FunctionPointerDemo/Debug/Src/subdir.mk
+++ FunctionPointerDemo/Debug/Src/subdir.mk
@@ -25,7 +25,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F429I_DISC1 -DSTM32 -DSTM32F429ZITx -DSTM32F4 -c -I../Inc -I../inc/STM32F4Headers -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F429I_DISC1 -DSTM32 -DSTM32F429ZITx -DSTM32F4 -c -I../Inc -I../inc/STM32F4Headers -I"/home/neill/Documents/GitHub/ConflictResolutionPractice/FunctionPointerDemo/Inc/STM32F4Headers" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Src
 
