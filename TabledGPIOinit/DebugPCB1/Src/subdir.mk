69ed4f152af0394031c18984932d8494a210ad9f
neill lambert <neillsyard@yahoo.com>
2023-12-11 21:27:13 +0000

    my tinkering

--- TabledGPIOinit/DebugPCB1/Src/subdir.mk
+++ TabledGPIOinit/DebugPCB1/Src/subdir.mk
@@ -5,25 +5,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/GPIO_Sys.c \
-../Src/SysTick.c \
-../Src/main.c \
-../Src/syscalls.c \
-../Src/sysmem.c 
+../Src/GPIO_Sys.c 
 
 OBJS += \
-./Src/GPIO_Sys.o \
-./Src/SysTick.o \
-./Src/main.o \
-./Src/syscalls.o \
-./Src/sysmem.o 
+./Src/GPIO_Sys.o 
 
 C_DEPS += \
-./Src/GPIO_Sys.d \
-./Src/SysTick.d \
-./Src/main.d \
-./Src/syscalls.d \
-./Src/sysmem.d 
+./Src/GPIO_Sys.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -33,7 +21,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/GPIO_Sys.cyclo ./Src/GPIO_Sys.d ./Src/GPIO_Sys.o ./Src/GPIO_Sys.su ./Src/SysTick.cyclo ./Src/SysTick.d ./Src/SysTick.o ./Src/SysTick.su ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/GPIO_Sys.cyclo ./Src/GPIO_Sys.d ./Src/GPIO_Sys.o ./Src/GPIO_Sys.su
 
 .PHONY: clean-Src
 
